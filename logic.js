d19c55d274238c1186f9597e8ba287519f2844a7
grupo6calsoft <grupo6cal.soft@gmail.com>
2024-07-08 04:40:42 -0500

    Update logic.js

--- logic.js
+++ logic.js
@@ -90,24 +90,6 @@ try {
     }
 }
 
-//class CalculadoraCientifica extends CalculadoraBasica {
-
-  //  constructor() {
-        // this.inputList = [];
-       // this.operationString = "";
-       // this.justSolved = false;
-       // this.operationMap = {
-          //  "sin(": "Math.sin(",
-          //  "cos(": "Math.cos(",
-          //  "tan(": "Math.tan(",
-          //  "log(": "Math.log10(",
-         //   "ln(": "Math.log(",
-           // "sqrt(": "Math.sqrt(",
-          //  "PI": "Math.PI",
-         //   "e": "Math.E"
-      //  };
-   // }
-
     writeToDisplay(data) {
         if (document.getElementById("displayBox").value === "Syntax Error") {
             super.clearDisplay();
